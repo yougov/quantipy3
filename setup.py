fc13c021dda970b04d804be49f9aeb8e64e33c53
Michal Jamry <jamrymi@gmail.com>
2022-08-01 13:33:49 +0200

    Update setup.py
    setup.py

--- setup.py
+++ setup.py
@@ -3,44 +3,25 @@
 import sys
 from setuptools import setup, find_packages
 
-versions = dict(numpy,
-                scipy,
-                pandas,
-                ftfy,
-                pyreadstat)
+INSTALL_REQUIRES = [
+  'numpy',
+  'scipy',
+  'pandas',
+  'ftfy',
+  'xmltodict',
+  'lxml',
+  'xlsxwriter',
+  'prettytable',
+  'decorator',
+  'watchdog',
+  'requests',
+  'python-pptx',
+  'pyreadstat'
+]
 
-precisions = dict(numpy='==',
-                  scipy='==',
-                  pandas='==',
-                  ftfy='==')
-
-libs = ['numpy',
-        'scipy',
-        'pandas',
-        'ftfy',
-        'xmltodict',
-        'lxml',
-        'xlsxwriter',
-        # 'pillow',
-        'prettytable',
-        'decorator',
-        'watchdog',
-        'requests',
-        'python-pptx',
-        'pyreadstat']
-
-def version_libs(libs, precisions, versions):
-    return [lib + precisions[lib] + versions[lib]
-            if lib in versions.keys() else lib
-            for lib in libs]
-
-if sys.platform == 'win32':
-    INSTALL_REQUIRES = version_libs(libs[2:], precisions, versions)
-else:
-    INSTALL_REQUIRES = version_libs(libs, precisions, versions)
 
 setup(name='quantipy3',
-      version='0.2.11',
+      version='0.3.0',
       author='Geir Freysson',
       author_email='geir@datasmoothie.com',
       packages=find_packages(exclude=['tests']),
